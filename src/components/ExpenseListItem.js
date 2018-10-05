ada47521f400870f8bf6ece1a6e44bbc9260fb69
philip <killfilipok@gmail.com>
2018-10-06 00:24:58 +0300

    SetUpAmountFormat

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,9 +1,16 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import moment from 'moment'
+import numeral from 'numeral'
 
-export default ({description, amount, createdAt,id}) => (
+export default ({ description, amount, createdAt, id }) => (
     <div>
         <Link to={`/edit/${id}`}>Expense List Item: </Link>
-        <p>{description} - {amount} created at: {createdAt}</p>
+        <p>
+             {description} 
+            - 
+            {numeral(amount /100).format('$0,0.00')} 
+            created at: {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>
 )
